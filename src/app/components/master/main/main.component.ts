bf5dc0f1a330ba187eaddad895a415d52a930f13
camreyaro <camreyaro@alum.us.es>
2020-03-29 13:43:46 +0200

    translated main page 🥕

--- src/app/components/master/main/main.component.ts
+++ src/app/components/master/main/main.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { TranslateService } from '@ngx-translate/core';
+import { TranslatableComponent } from '../../shared/translatable/translatable.component';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent extends TranslatableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private translateService: TranslateService) {
+    super(translateService);
+  }
 
   ngOnInit() {
   }
