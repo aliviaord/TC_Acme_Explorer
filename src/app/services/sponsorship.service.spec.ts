abb84c8433bc04269c1f8d03a73761d25980dbb3
Alicia <aliviaord@alum.us.es>
2020-04-19 14:16:11 +0200

    All testing errors fixed 🎉

--- src/app/services/sponsorship.service.spec.ts
+++ src/app/services/sponsorship.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SponsorshipService } from './sponsorship.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('SponsorshipService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: SponsorshipService = TestBed.get(SponsorshipService);
--- src/app/services/trip-application.service.spec.ts
+++ src/app/services/trip-application.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TripApplicationService } from './trip-application.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('TripApplicationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule
+    ]
+  }));
 
   it('should be created', () => {
     const service: TripApplicationService = TestBed.get(TripApplicationService);
