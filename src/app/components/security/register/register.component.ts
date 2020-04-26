978f7f6f695f588120fe82154b01b20dfb915aa6
camreyaro <camreyaro@alum.us.es>
2020-04-26 16:39:22 +0200

    added map to register form 🗺️

--- src/app/components/security/register/register.component.ts
+++ src/app/components/security/register/register.component.ts
@@ -4,6 +4,10 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslatableComponent } from '../../shared/translatable/translatable.component';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
 
 @Component({
   selector: 'app-register',
@@ -14,6 +18,7 @@ export class RegisterComponent extends TranslatableComponent implements OnInit {
 
   registrationForm: FormGroup;
   roleList: string[];
+  map: Map;
 
   constructor(private router: Router,
     private authService: AuthService,
@@ -25,6 +30,28 @@ export class RegisterComponent extends TranslatableComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: [213079.7791264898, 4929220.284081122],
+        zoom: 5
+      }),
+    });
+  }
+
+  getCoord(event: any){
+    var coordinate = this.map.getEventCoordinate(event);
+    console.log(coordinate)
   }
 
   createForm() {
